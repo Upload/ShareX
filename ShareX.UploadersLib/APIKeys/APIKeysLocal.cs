b4dc900c6965505b5d12962c2043adb682af9d14
ultra <ultra@localhost>
2015-06-05 20:15:17 -0400

    Added latest API keys from ShareX back in

---
+++ ShareX.UploadersLib/APIKeys/APIKeysLocal.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShareX.UploadersLib
+{
+    public static partial class APIKeys
+    {
+        static APIKeys()
+        {
+            APIKeys.ImageShackKey = "5DEFHIMQ81951c9e786e00f21774fec39382b6ad";
+            APIKeys.TinyPicID = "e2aabb8d555322fa";
+            APIKeys.TinyPicKey = "00a68ed73ddd54da52dc2d5803fa35ee";
+            APIKeys.ImgurClientID = "d297fd441566f99";
+            APIKeys.ImgurClientSecret = "0cfdf9368cbf988e69dff262d0425debd440fcc2";
+            APIKeys.FlickrKey = "fbe042faa01e6af0371b6d87be75436a";
+            APIKeys.FlickrSecret = "9f85687dcccc7ef9";
+            APIKeys.PhotobucketConsumerKey = "149828681";
+            APIKeys.PhotobucketConsumerSecret = "d2638b653e88315aac528087e9db54e3";
+            APIKeys.TwitsnapsKey = "694aaea3bc8f28479c383cbcc094fb55";
+            APIKeys.TwitPicKey = "816528c0152003a64e45402370805a5a";
+            APIKeys.DropboxConsumerKey = "0te7j9ype9lrdfn";
+            APIKeys.DropboxConsumerSecret = "r5d3aptd9a0cwp9";
+            APIKeys.MinusConsumerKey = "b57b69843f7a302a276dde89890fc6";
+            APIKeys.MinusConsumerSecret = "3fb097f08314d713959b1f41d543b0";
+            APIKeys.BoxClientID = "5vd8549xmn9a7zil4iupb0ciroj7cvee";
+            APIKeys.BoxClientSecret = "ghibKYH2nFSkfl6in4Rr2H9qGsiZHViY";
+            APIKeys.SendSpaceKey = "LV6OS1R0Q3";
+            APIKeys.Ge_ttKey = "tdrrlb84kq2dquxrh4dw79gr3act0529";
+            APIKeys.JiraConsumerKey = "NJgrJ4EhAahDwYuxLTtT";
+            APIKeys.CopyConsumerKey = "OgfXHS0yKaMGHo43Nhp0Ft3EJbvJ1JAn";
+            APIKeys.CopyConsumerSecret = "rbN1inAsx5M2slBHESJFhiapTdgZlQ02szCSBoQ0spvjDW3C";
+            APIKeys.MediaFireAppId = "41957";
+            APIKeys.MediaFireApiKey = "t12h2z1i7aui9boh7ttlx3j8549ufb3hje16k6ty";
+            APIKeys.OneDriveClientID = "000000004013BA0E";
+            APIKeys.OneDriveClientSecret = "Wd54VgTnoHq8Tn68VC026bxrKL6bwvBk";
+            APIKeys.HubicClientID = "api_hubic_C4QsXiTkR1lhkKZbtMWZBoKwpOe42aDa";
+            APIKeys.HubicClientSecret = "6CyB99tAsImQcBpMBOgdDEbHdGswGVgRNXyBKUcXTs3NTY52GDaENnyrVWMY0sQW";
+            APIKeys.PastebinKey = "4b23be71ec78bbd4fb96735320aa09ef";
+            APIKeys.PastebinCaKey = "KxTofLKQThSBZ63Gpa7hYLlMdyQlMD6u";
+            APIKeys.GitHubID = "badfa7b5e0a67e31da88";
+            APIKeys.GitHubSecret = "798dacd8f607cbc0777f6b90bd7c447da17263e8";
+            APIKeys.BitlyClientID = "0bf9726bea922ab7d5760a913d6749b441e5176a";
+            APIKeys.BitlyClientSecret = "fac46280c197865786a0af203f77d10abc3f533e";
+            APIKeys.GoogleAPIKey = "AIzaSyCAawIgkVq9ftQUY06rSdtN4LDbBdGJ3FE";
+            APIKeys.GoogleClientID = "810697162603-ag350u1fnmf3riubv91otme0v5fkk2d6.apps.googleusercontent.com";
+            APIKeys.GoogleClientSecret = "mDft-efE0PUcwIRCLG8nkLD9";
+            APIKeys.TwitterConsumerKey = "lZWoBpWcpJUKYbGGO7bl4S1ys";
+            APIKeys.TwitterConsumerSecret = "HyqkOlGkS4sORHRtBIUsL6Kit70OvJ3lgr784pomDQ6tijT0hg";
+        }
+    }
+}

fd52013dac46c9600e9efddbc48e79fe71781c7d
ultra <ultra@localhost>
2015-07-06 19:33:53 -0400

    Quick intermediate to get current users up to latest official ShareX with Up1 support.

--- ShareX/TaskSettings.cs
+++ ShareX/TaskSettings.cs
@@ -52,11 +52,11 @@ namespace ShareX
         public AfterUploadTasks AfterUploadJob = AfterUploadTasks.CopyURLToClipboard;
 
         public bool UseDefaultDestinations = true;
-        public ImageDestination ImageDestination = ImageDestination.Imgur;
-        public FileDestination ImageFileDestination = FileDestination.Dropbox;
-        public TextDestination TextDestination = TextDestination.Pastebin;
-        public FileDestination TextFileDestination = FileDestination.Dropbox;
-        public FileDestination FileDestination = FileDestination.Dropbox;
+        public ImageDestination ImageDestination = ImageDestination.FileUploader;
+        public FileDestination ImageFileDestination = FileDestination.Up1;
+        public TextDestination TextDestination = TextDestination.FileUploader;
+        public FileDestination TextFileDestination = FileDestination.Up1;
+        public FileDestination FileDestination = FileDestination.Up1;
         public UrlShortenerType URLShortenerDestination = UrlShortenerType.BITLY;
         public URLSharingServices URLSharingServiceDestination = URLSharingServices.Twitter;
 
